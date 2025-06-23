9dcefe7d9605ad77bc2dc4f2d9abdc835f0dc4f2
jason <rodriguezjasonlloyd@gmail.com>
2025-06-23 09:21:39 +0800

    chore: update treesitter config to new main branch

--- nvim/lua/plugins/code/treesitter.lua
+++ nvim/lua/plugins/code/treesitter.lua
@@ -2,39 +2,47 @@ return {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     build = ":TSUpdate",
-    lazy = false,
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "bash",
-                "c",
-                "diff",
-                "html",
-                "javascript",
-                "jsdoc",
-                "json",
-                "jsonc",
-                "lua",
-                "luadoc",
-                "luap",
-                "luau",
-                "markdown",
-                "markdown_inline",
-                "printf",
-                "python",
-                "query",
-                "regex",
-                "rust",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "xml",
-                "yaml",
-            },
-            auto_install = true,
-            highlight = { enable = true },
-        })
+        local treesitter = require("nvim-treesitter")
+        treesitter.setup()
+
+        local ensure_installed = {
+            "bash",
+            "c",
+            "diff",
+            "html",
+            "javascript",
+            "jsdoc",
+            "json",
+            "jsonc",
+            "lua",
+            "luadoc",
+            "luap",
+            "luau",
+            "markdown",
+            "markdown_inline",
+            "printf",
+            "python",
+            "query",
+            "regex",
+            "rust",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "xml",
+            "yaml",
+        }
+
+        local installed = require("nvim-treesitter.config").get_installed("parsers")
+
+        local not_installed = vim.tbl_filter(function(parser)
+            return not vim.tbl_contains(installed, parser)
+        end, ensure_installed)
+
+        if #not_installed > 0 then
+            require("nvim-treesitter").install(not_installed)
+        end
     end,
 }
