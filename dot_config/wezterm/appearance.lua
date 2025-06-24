da65dfdabb7a1f2f0ca7bac0044c4354355653cc
jason <rodriguezjasonlloyd@gmail.com>
2025-06-24 08:30:30 +0800

    feat: add more fonts

--- dot_config/wezterm/appearance.lua
+++ dot_config/wezterm/appearance.lua
@@ -3,7 +3,12 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.color_scheme = "nord"
-config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
+config.font = wezterm.font_with_fallback({
+    { family = "JetBrainsMono Nerd Font", weight = "Bold" },
+    { family = "MesloLGS Nerd Font Mono", weight = "Bold" },
+    { family = "Hack Nerd Font Mono", weight = "Bold" },
+    { family = "SauceCodePro Nerd Font Mono", weight = "Bold" },
+})
 config.font_size = 12.0
 config.harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" }
 
