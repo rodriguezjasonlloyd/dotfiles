0431b8e250156eff008636e75e97152a36904e39
jason <rodriguezjasonlloyd@gmail.com>
2025-06-25 05:04:36 +0800

    feat: center wezterm on launch

--- dot_config/wezterm/windows.lua
+++ dot_config/wezterm/windows.lua
@@ -1,3 +1,5 @@
+local wezterm = require("wezterm")
+
 local config = {}
 
 config.initial_cols = 110
@@ -9,5 +11,13 @@ config.window_padding = {
     top = 0,
     bottom = 0,
 }
+
+wezterm.on("gui-startup", function()
+    local screen = wezterm.gui.screens().active
+    local _, _, window = wezterm.mux.spawn_window({ origin = "ActiveScreen" })
+    local gui_window = window:gui_window()
+    local dimensions = gui_window:get_dimensions()
+    gui_window:set_position((screen.width - dimensions.pixel_width) / 2, (screen.height - dimensions.pixel_height) / 2)
+end)
 
 return config
