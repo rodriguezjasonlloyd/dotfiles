4ed3e9cf60583f6f82b42b15637189e23b0d6ec8
jason <rodriguezjasonlloyd@gmail.com>
2025-06-25 10:53:00 +0800

    feat(theme): swtich from nord to catppuccin

--- nvim/lua/plugins/appearance/theme.lua
+++ nvim/lua/plugins/appearance/theme.lua
@@ -1,10 +1,12 @@
 return {
-    "shaunsingh/nord.nvim",
-    lazy = false,
-    config = function()
-        vim.g.nord_contrast = true
-        vim.g.nord_borders = true
-        require("nord").set()
-    end,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "macchiato",
+        })
+
+        vim.cmd.colorscheme("catppuccin")
+    end,
 }
