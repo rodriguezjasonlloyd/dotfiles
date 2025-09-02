02aa18a1adbe054d23db77f76d14856d65581b9b
jason <rodriguezjasonlloyd@gmail.com>
2025-09-02 14:33:19 +0800

    feat(wezterm): add paddings

--- dot_config/wezterm/windows.lua
+++ dot_config/wezterm/windows.lua
@@ -5,12 +5,6 @@ local config = {}
 config.initial_cols = 110
 config.initial_rows = 36
 config.window_decorations = "RESIZE"
-config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-}
 
 wezterm.on("gui-startup", function()
     local screen = wezterm.gui.screens().active
