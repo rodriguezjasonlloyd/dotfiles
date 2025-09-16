df1bf1939915ad47b3afe912e8f5683181630bba
jason <rodriguezjasonlloyd@gmail.com>
2025-09-17 07:37:49 +0800

    refactor(wezterm): reorder fonts

--- wezterm/appearance.lua
+++ wezterm/appearance.lua
@@ -4,10 +4,10 @@ local config = {}
 
 config.color_scheme = "ayu"
 config.font = wezterm.font_with_fallback({
-    { family = "JetBrainsMono Nerd Font", weight = "Bold" },
-    { family = "MesloLGS Nerd Font Mono", weight = "Bold" },
-    { family = "Hack Nerd Font Mono", weight = "Bold" },
-    { family = "SauceCodePro Nerd Font Mono", weight = "Bold" },
+    { family = "Hack Nerd Font Mono", weight = "Regular" },
+    { family = "MesloLGS Nerd Font Mono", weight = "Regular" },
+    { family = "JetBrainsMono Nerd Font", weight = "Regular" },
+    { family = "SauceCodePro Nerd Font Mono", weight = "Regular" },
 })
 config.font_size = 12.0
 config.harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" }
