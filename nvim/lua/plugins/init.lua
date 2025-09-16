fb1de41bbeac7157a8711d8f0bb8eaa74e8b7a76
jason <rodriguezjasonlloyd@gmail.com>
2025-09-17 07:41:26 +0800

    fix(nvim): split up installs to prevent timeout

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,26 +1,34 @@
+-- Visuals
 vim.pack.add({
-    -- Visuals
     { src = "https://github.com/rose-pine/neovim" },
     { src = "https://github.com/nvim-tree/nvim-web-devicons" },
     { src = "https://github.com/MunifTanjim/nui.nvim" },
     { src = "https://github.com/nvim-lualine/lualine.nvim" },
     { src = "https://github.com/HiPhish/rainbow-delimiters.nvim" },
+}, { confirm = false })
 
-    -- Code
+-- Installers
+vim.pack.add({
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+}, { confirm = false })
+
+-- Editing
+vim.pack.add({
     { src = "https://github.com/stevearc/conform.nvim" },
     { src = "https://github.com/mfussenegger/nvim-lint" },
     { src = "https://github.com/folke/flash.nvim" },
     { src = "https://github.com/saghen/blink.cmp" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
     { src = "https://github.com/windwp/nvim-autopairs" },
     { src = "https://github.com/windwp/nvim-ts-autotag" },
     { src = "https://github.com/RRethy/nvim-treesitter-endwise" },
     { src = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring" },
     { src = "https://github.com/folke/ts-comments.nvim" },
+}, { confirm = false })
 
-    -- Miscellaneous
+-- Miscellaneous
+vim.pack.add({
     { src = "https://github.com/stevearc/oil.nvim" },
     { src = "https://github.com/folke/which-key.nvim" },
     { src = "https://github.com/folke/persistence.nvim" },
@@ -28,7 +36,7 @@ vim.pack.add({
     { src = "https://github.com/vyfor/cord.nvim" },
     { src = "https://github.com/folke/snacks.nvim" },
     { src = "https://github.com/folke/noice.nvim" },
-})
+}, { confirm = false })
 
 require("plugins.visuals")
 require("plugins.snacks")
