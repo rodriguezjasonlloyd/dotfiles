124adc270727898bfe44602e4e1471bb8c0240cd
jason <rodriguezjasonlloyd@gmail.com>
2026-02-07 21:28:56 +0800

    feat(nvim): update blink.cmp

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -1,5 +1,5 @@
 require("blink.cmp").setup({
-    fuzzy = { prebuilt_binaries = { force_version = "v1.7.0" } },
+    fuzzy = { prebuilt_binaries = { force_version = "v1.9.1" } },
     completion = { documentation = { auto_show = true } },
     keymap = { preset = "super-tab" },
 })
